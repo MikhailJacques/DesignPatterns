770d1d52e7c0366509a3dffc806600798edfb50d
Mikhail Jacques <mikhailjacques@gmail.com>
2022-09-21 15:55:28 +0300

    Update Factory.cpp

--- Factory/Factory.cpp
+++ Factory/Factory.cpp
@@ -2,6 +2,13 @@
 // Factory Method is a creational design pattern that provides an interface for creating objects in a superclass, 
 // but allows subclasses to alter the type of objects that will be created.
 // 
+// Usage examples: The Factory Method pattern is widely used in C++ code. 
+// It is very useful when you need to provide a high level of flexibility for your code.
+
+// Identification: Factory methods can be recognized by creation methods that construct objects 
+// from concrete classes. While concrete classes are used during the object creation, 
+// the return type of the factory methods is usually declared as either an abstract class or an interface.
+// 
 // https://refactoring.guru/design-patterns/factory-method
 
 #include <mutex>
